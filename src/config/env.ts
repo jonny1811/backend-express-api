3c6472ea04a565040fa3dd616b3596c581c0a8b1
jonny1811 <jonny@quanto.la>
2022-01-09 12:34:53 -0300

    config: add postman collection and config env on routes

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,5 @@
 export default {
-    port: process.env.PORT || 9000
+    port: process.env.PORT || 9000,
+    baseUrl: process.env.BASE_URL || '/api',
+    docsUrl: process.env.DOCS_URL || '/api/docs',
 }
